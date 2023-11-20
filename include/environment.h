5526f6399e8892040473c6c00041b2edde98b5fa
Joseph Hunkeler <jhunkeler@gmail.com>
2023-11-20 10:15:51 -0500

    runtime_set takes pointer to char, instead of constant pointer to char

--- include/environment.h
+++ include/environment.h
@@ -15,7 +15,7 @@ ssize_t runtime_contains(RuntimeEnv *env, const char *key);
 RuntimeEnv *runtime_copy(char **env);
 int runtime_replace(RuntimeEnv **dest, char **src);
 char *runtime_get(RuntimeEnv *env, const char *key);
-void runtime_set(RuntimeEnv *env, const char *_key, const char *_value);
+void runtime_set(RuntimeEnv *env, const char *_key, char *_value);
 char *runtime_expand_var(RuntimeEnv *env, char *input);
 void runtime_export(RuntimeEnv *env, char **keys);
 void runtime_apply(RuntimeEnv *env);
