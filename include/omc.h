9c921f7cce985ec59420793b2f6388b980fcfe5d
Joseph Hunkeler <jhunkeler@gmail.com>
2024-03-22 11:25:48 -0400

    Add GENERIC_ARRAY_FREE macro to avoid code duplications when freeing character arrays

--- include/omc.h
+++ include/omc.h
@@ -41,6 +41,12 @@
 #define guard_runtime_free(X) do { if (X) { runtime_free(X); X = NULL; } } while (0)
 #define guard_strlist_free(X) do { if (X) { strlist_free(X); X = NULL; } } while (0)
 #define guard_free(X) do { if (X) { free(X); X = NULL; } } while (0)
+#define GENERIC_ARRAY_FREE(ARR) do { \
+    for (size_t ARR_I = 0; ARR && ARR[ARR_I] != NULL; ARR_I++) { \
+        guard_free(ARR[ARR_I]); \
+    } \
+    guard_free(ARR); \
+} while (0)
 
 #define COE_CHECK_ABORT(COND, MSG) {\
     if (COND) { \
