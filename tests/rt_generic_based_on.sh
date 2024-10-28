87840f958af165e0d8ac65446c3d9a3a5266902e
Joseph Hunkeler <jhunkeler@gmail.com>
2024-10-28 13:24:27 -0400

    Add rt_generic_based_on.sh
    * Tests the "based_on" key

---
+++ tests/rt_generic_based_on.sh
@@ -0,0 +1,29 @@
+here="$(dirname ${BASH_SOURCE[0]})"
+source $here/setup.sh
+
+TEST_NAME=generic_based_on
+PYTHON_VERSIONS=(
+    3.11
+)
+setup_workspace "$TEST_NAME"
+install_stasis
+
+ln -s "$TOPDIR"/"$TEST_NAME".yml
+for py_version in "${PYTHON_VERSIONS[@]}"; do
+    run_stasis --python "$py_version" \
+        --no-docker \
+        --no-artifactory \
+        "$TOPDIR"/"$TEST_NAME".ini
+done
+
+check_output_add "(null)"
+check_output_stasis_dir stasis/*/output
+check_output_reset
+
+# NOTE: indexer default output directory is "output"
+check_output_add "(null)"
+run_stasis_indexer stasis
+check_output_indexed_dir output
+check_output_reset
+
+teardown_workspace "$TEST_NAME"
