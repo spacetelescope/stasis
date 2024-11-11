23f96e574b756339911103816c3b1a582a27f69b
Joseph Hunkeler <jhunkeler@gmail.com>
2024-11-11 13:08:19 -0500

    Change PREFIX to INSTALL_DIR
    * Conda uses PREFIX (or can use it) so it's better to avoid overriding it.

--- tests/setup.sh
+++ tests/setup.sh
@@ -46,8 +46,8 @@ setup_workspace() {
     fi
     WORKSPACE="$(realpath $WORKSPACE)"
     
-    export PREFIX="$WORKSPACE"/local
-    if ! mkdir -p "$PREFIX"; then
+    export INSTALL_DIR="$WORKSPACE"/local
+    if ! mkdir -p "$INSTALL_DIR"; then
         echo "directory creation failed. cannot continue" >&2
         return 1;
     fi
@@ -78,7 +78,7 @@ teardown_workspace() {
 
 install_stasis() {
     pushd "$BUILD_DIR"
-    if ! cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Debug "${TOPDIR}"/../..; then
+    if ! cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DCMAKE_BUILD_TYPE=Debug "${TOPDIR}"/../..; then
         echo "cmake failed" >&2
         return 1
     fi
@@ -88,7 +88,7 @@ install_stasis() {
         return 1
     fi
 
-    export PATH="$PREFIX/bin:$PATH"
+    export PATH="$INSTALL_DIR/bin:$PATH"
     hash -r
     if ! type -P stasis; then
         echo "stasis program not on PATH" >&2
