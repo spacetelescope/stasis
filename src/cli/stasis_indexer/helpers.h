47b0eb0ffc358393238a2e4ec8600f5a862a99b5
Joseph Hunkeler <jhunkeler@gmail.com>
2024-12-06 08:32:36 -0500

    Export pandoc_exec()

--- src/cli/stasis_indexer/helpers.h
+++ src/cli/stasis_indexer/helpers.h
@@ -3,9 +3,20 @@
 
 #include "delivery.h"
 
+#define ARRAY_COUNT_DYNAMIC(X, COUNTER) \
+    do { \
+        for (COUNTER = 0; X && X[COUNTER] != NULL; COUNTER++) {} \
+    } while(0)
+
+#define ARRAY_COUNT_BY_STRUCT_MEMBER(X, MEMBER, COUNTER) \
+    do { \
+        for (COUNTER = 0; X[COUNTER].MEMBER != NULL; COUNTER++) {} \
+    } while(0)
+
 struct StrList *get_architectures(struct Delivery ctx[], size_t nelem);
 struct StrList *get_platforms(struct Delivery ctx[], size_t nelem);
 int get_pandoc_version(size_t *result);
+int pandoc_exec(const char *in_file, const char *out_file, const char *css_file, const char *title);
 int get_latest_rc(struct Delivery ctx[], size_t nelem);
 struct Delivery **get_latest_deliveries(struct Delivery ctx[], size_t nelem);
 int get_files(struct StrList **out, const char *path, const char *pattern, ...);
