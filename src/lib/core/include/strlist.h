727e176e30cf05ee2d88c28bb44bb0364d8e4e72
Joseph Hunkeler <jhunkeler@gmail.com>
2025-01-31 11:23:50 -0500

    Implements function strlist_contains

--- src/lib/core/include/strlist.h
+++ src/lib/core/include/strlist.h
@@ -40,6 +40,7 @@ void strlist_set(struct StrList **pStrList, size_t index, char *value);
 size_t strlist_count(struct StrList *pStrList);
 void strlist_reverse(struct StrList *pStrList);
 void strlist_sort(struct StrList *pStrList, unsigned int mode);
+int strlist_contains(struct StrList *pStrList, const char *value, size_t *index_of);
 int strlist_append_file(struct StrList *pStrList, char *path, ReaderFn *readerFn);
 void strlist_append_strlist(struct StrList *pStrList1, struct StrList *pStrList2);
 void strlist_append(struct StrList **pStrList, char *str);
