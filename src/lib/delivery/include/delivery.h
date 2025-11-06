891059058771282b181ca06b2312249de129afbe
Joseph Hunkeler <jhunkeler@gmail.com>
2025-11-06 08:56:43 -0500

    Delivery context is constant

--- src/lib/delivery/include/delivery.h
+++ src/lib/delivery/include/delivery.h
@@ -486,6 +486,6 @@ void delivery_rewrite_stage2(struct Delivery *ctx, char *specfile);
  * @param ctx Delivery context
  * @return a copy
  */
-struct Delivery *delivery_duplicate(struct Delivery *ctx);
+struct Delivery *delivery_duplicate(const struct Delivery *ctx);
 
 #endif //STASIS_DELIVERY_H
