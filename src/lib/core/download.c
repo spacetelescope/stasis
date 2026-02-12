4458f2e9da2ef474c63457a96ee0f637fa89ad14
Joseph Hunkeler <jhunkeler@gmail.com>
2026-02-12 09:51:58 -0500

    Use ssize_t to avoid rollover from strtol on user input

--- src/lib/core/download.c
+++ src/lib/core/download.c
@@ -24,7 +24,7 @@ long download(char *url, const char *filename, char **errmsg) {
         }
     }
 
-    size_t max_retries = 5;
+    ssize_t max_retries = 5;
     const char *max_retries_str = getenv("STASIS_DOWNLOAD_RETRY_MAX");
     if (max_retries_str) {
         max_retries = strtol(max_retries_str, NULL, 10);
@@ -33,7 +33,7 @@ long download(char *url, const char *filename, char **errmsg) {
         }
     }
 
-    size_t max_retry_seconds = 3;
+    ssize_t max_retry_seconds = 3;
     const char *max_retry_seconds_str = getenv("STASIS_DOWNLOAD_RETRY_SECONDS");
     if (max_retry_seconds_str) {
         max_retry_seconds = strtol(max_retry_seconds_str, NULL, 10);
@@ -45,7 +45,7 @@ long download(char *url, const char *filename, char **errmsg) {
 
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *c = curl_easy_init();
-    for (size_t retry = 0; retry < max_retries; retry++) {
+    for (ssize_t retry = 0; retry < max_retries; retry++) {
         if (retry) {
             fprintf(stderr, "[RETRY %zu/%zu] %s: %s\n", retry + 1, max_retries, *errmsg, url);
         }
