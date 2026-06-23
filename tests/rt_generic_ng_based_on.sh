9155a2159f1ed057e657a1418dd9c4f3696223a1
Joseph Hunkeler <jhunkeler@gmail.com>
2026-06-22 21:55:04 -0400

    Add GBO test using a modern conda installer

---
+++ tests/rt_generic_ng_based_on.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+here="$(dirname ${BASH_SOURCE[0]})"
+source $here/setup.sh
+
+TEST_NAME=gbo_ng
+PYTHON_VERSIONS=(
+    3.11
+)
+setup_workspace "$TEST_NAME"
+run_command install_stasis
+
+ln -s "$TEST_DATA"/"$TEST_NAME".yml
+for py_version in "${PYTHON_VERSIONS[@]}"; do
+    run_command run_stasis --python "$py_version" \
+        --no-docker \
+        --no-artifactory \
+        "$TEST_DATA"/"$TEST_NAME".ini
+done
+
+check_output_add "(null)"
+run_command check_output_stasis_dir stasis/*/output
+check_output_reset
+
+# NOTE: indexer default output directory is "output"
+check_output_add "(null)"
+run_command run_stasis_indexer stasis
+run_command check_output_indexed_dir output
+check_output_reset
+
+teardown_workspace "$TEST_NAME"
